6e0f072887f4a129fb3147a9c7beb9c424484795
CHowardTsou <127212524+CHowardTsou@users.noreply.github.com>
2024-05-13 19:59:01 +0800

    update draggable

--- index.pack.js
+++ index.pack.js
@@ -393,6 +393,9 @@ if (process.env.NODE_ENV === 'production') {
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
+
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 exports.default = App;
 
 var _react = __webpack_require__(1);
@@ -413,9 +416,47 @@ var _data2 = _interopRequireDefault(_data);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 function App() {
-    var list = _data2.default.map(function (item) {
-        return _react2.default.createElement(_Card2.default, { key: item.id, item: item });
+    var _React$useState = _react2.default.useState(_data2.default),
+        _React$useState2 = _slicedToArray(_React$useState, 2),
+        items = _React$useState2[0],
+        setItems = _React$useState2[1];
+
+    var handleDragStart = function handleDragStart(e, index) {
+        e.dataTransfer.setData('index', index);
+    };
+
+    var handleDragOver = function handleDragOver(e) {
+        e.preventDefault();
+    };
+
+    var handleDrop = function handleDrop(e, index) {
+        e.preventDefault();
+        var droppedIndex = e.dataTransfer.getData('index');
+        var newItems = [].concat(_toConsumableArray(items));
+        var temp = newItems[index];
+        newItems[index] = newItems[droppedIndex];
+        newItems[droppedIndex] = temp;
+        setItems(newItems);
+    };
+
+    var list = items.map(function (item, index) {
+        return _react2.default.createElement(
+            'div',
+            { className: 'item',
+                draggable: true,
+                key: index,
+                onDragStart: function onDragStart(e) {
+                    return handleDragStart(e, index);
+                },
+                onDragOver: handleDragOver,
+                onDrop: function onDrop(e) {
+                    return handleDrop(e, index);
+                } },
+            _react2.default.createElement(_Card2.default, { key: item.id, item: item })
+        );
     });
     return _react2.default.createElement(
         'div',
@@ -577,7 +618,7 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = [{
     id: 1,
-    title: "Mount Fuji",
+    title: "Mount Fujii",
     location: "Japan",
     googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
     startDate: "12 Jan, 2021",
